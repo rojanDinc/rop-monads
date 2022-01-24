4beab37a086a7d48d608087cd5853b1f98045cb5
rojan dinc <rojand94@gmail.com>
2022-01-24 20:40:04 +0100

    Update Optional documentation

--- src/lib/optional.ts
+++ src/lib/optional.ts
@@ -3,14 +3,29 @@ type None = null;
 export class Optional<T> {
   private constructor(private readonly value: T | None) {}
 
+  /**
+   * Create an `Optional` instance with a wrapped none value.
+   * @returns Optional with none wrapped value.
+   */
   public static none<T>(): Optional<T> {
     return new Optional<T>(null);
   }
 
+  /**
+   * Create an `Optional` instance with a provided value.
+   * @param value
+   * @returns Optional with some wrapped value.
+   */
   public static some<T>(value: T): Optional<T> {
     return new Optional<T>(value);
   }
 
+  /**
+   * Create an `Optional` instance from provided value.
+   * This method will determine if the provided value is some or none.
+   * @param value
+   * @returns Optional with either some or none wrapped value.
+   */
   public static of<T>(value: T | None): Optional<T> {
     if (value === undefined || value === null) {
       return Optional.none();
@@ -18,14 +33,19 @@ export class Optional<T> {
     return Optional.some(value);
   }
 
-  private isNone(): boolean {
-    return this.value === undefined || this.value === null;
-  }
-
+  /**
+   * Get the wrapped optional value.
+   * @returns Wrapped value.
+   */
   public get(): T | None {
     return this.value;
   }
 
+  /**
+   * Transform wrapped value.
+   * @param fn Transforming function.
+   * @returns Optional with new wrapped value type.
+   */
   public map<U>(fn: (value: T) => U): Optional<U> {
     if (this.isNone()) {
       return Optional.none();
@@ -33,6 +53,11 @@ export class Optional<T> {
     return Optional.some(fn(this.value as T));
   }
 
+  /**
+   * Flatten a nested `Optional`.
+   * @param fn Flattening function that returns a new `Optional`.
+   * @returns New `Optional<U>` value.
+   */
   public flatMap<U>(fn: (value: T) => Optional<U>): Optional<U> {
     if (this.isNone()) {
       return Optional.none();
@@ -40,6 +65,11 @@ export class Optional<T> {
     return fn(this.value as T);
   }
 
+  /**
+   * Apply a function if the wrapped value is not none.
+   * @param fn Callback function.
+   * @returns Same `Optional` instance.
+   */
   public apply(fn: (value: T) => void): Optional<T> {
     if (!this.isNone()) {
       fn(this.value as T);
@@ -48,16 +78,26 @@ export class Optional<T> {
     return this;
   }
 
+  /**
+   * Filter on an `Optional`.
+   * @param predicate Predicate function which determines if returned value should return `this` or none.
+   * @returns Same instance or none.
+   */
   public filter(predicate: (value: T) => boolean): Optional<T> {
-    switch (this.isNone()) {
-      case true:
-        return this;
-      case false:
-        return predicate(this.value as T) ? this : Optional.none();
+    if (this.isNone()) {
+      return this;
     }
+
+    return predicate(this.value as T) ? this : Optional.none();
   }
 
-  public orElse(fn: () => T): T {
+  /**
+   * Run a callback function if wrapped value is none to give back a non none value.
+   * @typeParam T A non nullable type.
+   * @param fn Callback function.
+   * @returns Same instance.
+   */
+  public orElse(fn: () => NonNullable<T>): T {
     if (this.isNone()) {
       return fn();
     }
@@ -65,7 +105,14 @@ export class Optional<T> {
     return this.value as T;
   }
 
+  /**
+   * A property to determine if the wrapped value has a value.
+   */
   public get hasValue(): boolean {
     return this.isNone() === false;
+  }
+
+  private isNone(): boolean {
+    return this.value === undefined || this.value === null;
   }
 }
